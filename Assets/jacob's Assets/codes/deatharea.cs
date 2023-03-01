d37cd2156df30de55a024048042aeb031bb90046
nightmarejfoxb <118293178+nightmarejfoxb@users.noreply.github.com>
2023-03-01 09:56:48 -0800

    got moving
    the player is able to go up, down, right and left there is a idle but no turning

---
+++ Assets/jacob's Assets/codes/deatharea.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deatharea : MonoBehaviour
+{
+    public float lowerLimit;
+    public float upperLimit;
+    public float leftLimit;
+    public float rightLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < lowerLimit)
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().Respawn();
+        }
+        if (transform.position.y < upperLimit)
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().Respawn();
+        }
+        if (transform.position.x < leftLimit)
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().Respawn();
+        }
+        if (transform.position.x < rightLimit)
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().Respawn();
+        }
+    }
+}
